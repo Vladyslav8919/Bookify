6180de88f78621d560baf47c03d40ec555719c0e
Vladyslav8919 <vladyslav.pshevlotskyi@gmail.com>
2022-12-18 11:49:09 +0200

    Added countdown

--- script.js
+++ script.js
@@ -229,6 +229,94 @@ giftArticle.addEventListener("click", function (e) {
 
 /*
 -----------
+COUNTDOWN
+-----------
+*/
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const weekdays = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
+const giveawayDeadline = document.querySelector(".giveaway-deadline");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4");
+
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate();
+
+const futureDate = new Date(tempYear, tempMonth, tempDay + 2, 16, 24, 13);
+
+const year = futureDate.getFullYear();
+const month = months[futureDate.getMonth()];
+const weekday = weekdays[futureDate.getDay()];
+const day = futureDate.getDate();
+const hours = futureDate.getHours();
+const mins = futureDate.getMinutes();
+// const secs = futureDate.getSeconds();
+
+giveawayDeadline.textContent = `Giveaway ends on ${weekday} ${day} ${month} ${year} ${hours}:${mins}`;
+
+const getRemainingTime = function () {
+  const today = new Date().getTime();
+  const futureTime = futureDate.getTime();
+
+  const t = futureTime - today;
+
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+
+  const days = Math.floor(t / oneDay);
+  const hours = Math.floor((t % oneDay) / oneHour);
+  const minutes = Math.floor((t % oneHour) / oneMinute);
+  const seconds = Math.floor((t % oneMinute) / 1000);
+
+  const values = [days, hours, minutes, seconds];
+
+  const format = function (item) {
+    if (item < 10) {
+      return (item = `0${item}`);
+    }
+    return item;
+  };
+
+  items.forEach(function (item, i) {
+    item.textContent = format(values[i]);
+  });
+
+  if (t < 0) {
+    clearInterval(countdown);
+    deadline.innerHTML = `<h4 class="expired">Sorry, this giveaway has expired.</h4>`;
+  }
+};
+
+let countdown = setInterval(getRemainingTime, 1000);
+
+getRemainingTime();
+
+/*
+-----------
 QUESTIONS
 -----------
 */
